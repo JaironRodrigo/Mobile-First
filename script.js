db3d20753c10a6e3c757ffb2274ab281dd2cdd82
JaironRodrigo <jaironcreed@gmail.com>
2023-01-29 21:45:43 -0300

    Aplicando o CSS à pagina e criando a responsividade através dos media queries para mobile

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(".owl-carousel").owlCarousel({
   loop: true,
-  margin: 10,
+  margin: 20,
   nav: true,
+  dots: false,
+  stagePadding: 50,
+  autoplay: true,
+  autoplayTimeout: 3000,
   responsive: {
     0: {
-      items: 1,
+      items: 2,
     },
     600: {
       items: 3,
